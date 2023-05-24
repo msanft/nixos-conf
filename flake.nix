733d5e434915a167824e38fed3344daeec937f8a
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2023-05-24 19:08:35 +0200

    tidying

--- flake.nix
+++ flake.nix
@@ -1,20 +1,11 @@
 {
-  description = "Your new nix config";
+  description = "NixOS Configuration of msanft";
 
   inputs = {
-    # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
 
-    # Home manager
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    # TODO: Add any other flake you might need
-    # hardware.url = "github:nixos/nixos-hardware";
-
-    # Shameless plug: looking for a way to nixify your themes and make
-    # everything match nicely? Try nix-colors!
-    # nix-colors.url = "github:misterio77/nix-colors";
   };
 
   outputs = inputs @ {
