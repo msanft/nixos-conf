1eb0766ec21465c24270d11cdb212473fe9b0e05
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2023-05-26 09:11:03 +0200

    office kanshi setup

--- home/wm/sway/kanshi.nix
+++ home/wm/sway/kanshi.nix
@@ -5,11 +5,16 @@ let
     sizeY = "1440";
     refreshRate = "120Hz";
   };
+  dellP2720D = {
+    name = "Dell Inc. DELL P2720D";
+    sizeX = "2560";
+    sizeY = "1440";
+    refreshRate = "59.951Hz";
+  };
 in {
   services.kanshi = {
     enable = true;
     profiles = {
-
         docked.outputs = [
             {
                 criteria = "${dellG3223D.name} H916XG3";
@@ -28,6 +33,20 @@ in {
                 status = "disable";
                 position = "0,0";
             }
+        ];
+
+        office.outputs = [
+          {
+                criteria = "${dellP2720D.name} 2SMJK53";
+                status = "enable";
+                mode = "${dellP2720D.sizeX}x${dellP2720D.sizeY}@${dellP2720D.refreshRate}";
+                position = "0,0";
+            }
+          {
+              criteria = "eDP-1";
+              status = "enable";
+              position = "${dellP2720D.sizeX},0";
+          }
         ];
 
         undocked.outputs = [
