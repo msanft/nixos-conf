8b14f65ebd114e47b4a80c4e808ef9342d0a8613
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2023-06-10 09:50:00 +0200

    darwin system config

--- hosts/mb/configuration.nix
+++ hosts/mb/configuration.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ pkgs, system, ... }: {
   imports = [
     ../common
     ./brew.nix
@@ -17,4 +17,38 @@
       ../../home/mb
     ];
   };
+
+  system.defaults.NSGlobalDomain = {
+    "com.apple.swipescrolldirection" = false;
+    AppleInterfaceStyleSwitchesAutomatically = true;
+    NSAutomaticCapitalizationEnabled = false;
+    NSNavPanelExpandedStateForSaveMode = true;
+    NSNavPanelExpandedStateForSaveMode2 = true;
+    NSScrollAnimationEnabled = true;
+    PMPrintingExpandedStateForPrint = true;
+    PMPrintingExpandedStateForPrint2 = true;
+  };
+
+  system.defaults.LaunchServices.LSQuarantine = false;
+
+  system.defaults.dock = {
+    autohide = true;
+    minimize-to-application = true;
+    orientation = "left";
+    show-recents = false;
+  };
+
+  system.defaults.finder = {
+    AppleShowAllFiles = true;
+    AppleShowAllExtensions = true;
+    FXDefaultSearchScope = "SCcf"; # search current folder by default
+    FXEnableExtensionChangeWarning = false;
+    FXPreferredViewStyle = "clmv"; # column view
+    ShowPathbar = true;
+    ShowStatusBar = true;
+    _FxShowPosixPathInTitle = true;
+  };
+
+  system.defaults.menuExtraClock.ShowSeconds = true;
+  system.defaults.screencapture.location = "~/Pictures/Screenshots";
 }
