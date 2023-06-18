6e5f80a64f25dfb418893b7cbe0a6c4ed4a42a4a
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2023-06-18 21:17:49 +0200

    tuigreet customization

--- hosts/tp/configuration.nix
+++ hosts/tp/configuration.nix
@@ -127,11 +127,26 @@
         user = "moritzs";
         command = ''
           ${pkgs.greetd.tuigreet}/bin/tuigreet \
-          --container-padding 3 \
           --remember --time --asterisks \
           --cmd '${pkgs.sway}/bin/sway'
         '';
       };
+    };
+  };
+
+  environment.etc = {
+    "gruvbox-vt" = {
+      source = ./gruvbox-vt;
+      mode = "0777";
+    };
+  };
+
+  systemd.services."vt-colorscheme" = {
+    wantedBy = [ "multi-user.target" ];
+    description = "Adjust the VT colorscheme.";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = ''${pkgs.kbd}/bin/setvtrgb /etc/gruvbox-vt'';
     };
   };
 
