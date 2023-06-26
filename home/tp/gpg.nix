fab4e49e304b15b6d2d1bc15db4b3fd915d7e889
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2023-06-26 08:53:19 +0200

    wip: gpg

---
+++ home/tp/gpg.nix
@@ -0,0 +1,14 @@
+{config, pkgs, ...}: {
+  programs.gpg.enable = true;
+
+  services.gpg-agent = {
+    enable = true;
+    enableZshIntegration = true;
+    enableSshSupport = true;
+    pinentryFlavor = "curses";
+  };
+
+  home.packages = with pkgs; [
+    pinentry-curses
+  ];
+}
