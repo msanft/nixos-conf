ddc8440a2fe0bfb4f0b026d1e6d60453208becb8
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2023-08-20 20:06:01 +0200

    Signing

--- home/modules/git/default.nix
+++ home/modules/git/default.nix
@@ -1,9 +1,9 @@
 { pkgs, ... }: {
-  # home.packages = with pkgs; [
-  #     # required for commit signing
-  #     gnupg
-  #     pinentry
-  # ];
+  home.packages = with pkgs; [
+    # required for commit signing
+    gnupg
+    pinentry
+  ];
 
   programs.git = {
     enable = true;
@@ -11,15 +11,19 @@
     userName = "Moritz Sanft";
     userEmail = "58110325+msanft@users.noreply.github.com";
 
-    # signing = {
-    #     key = null;
-    #     signByDefault = true;
-    # };
+    signing = {
+      key = "899A193768C34EEA";
+      signByDefault = true;
+    };
 
     difftastic.enable = true;
 
     extraConfig = {
       init.defaultBranch = "main";
+      core.askPass = ""; # needs to be empty to use terminal for ask pass
     };
   };
+
+
+
 }
