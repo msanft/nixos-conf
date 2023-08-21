6779efac8694d2518546a2189fb8db7b0dd6bf51
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2023-08-21 20:50:35 +0200

    yubikey: add login functionality

--- modules/system/yubikey/default.nix
+++ modules/system/yubikey/default.nix
@@ -3,4 +3,11 @@
     pcscd.enable = true;
     udev.packages = [ pkgs.yubikey-personalization ];
   };
+
+  security.pam.yubico = {
+    enable = true;
+    debug = true;
+    mode = "challenge-response";
+    id = [ "25808654" ];
+  };
 }
