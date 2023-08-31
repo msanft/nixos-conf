6d08faef4b9e155d4382d57f3c2ad24bfb7ca232
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2023-08-31 19:45:33 +0200

    wip: nixvim
    Signed-off-by: Moritz Sanft <58110325+msanft@users.noreply.github.com>

--- modules/home/nixvim/default.nix
+++ modules/home/nixvim/default.nix
@@ -1,17 +1,34 @@
 { pkgs, ... }: {
+  home.packages = with pkgs; [
+    ripgrep
+  ];
+
   programs.nixvim = {
     enable = true;
+
+    options = {
+      number = true;
+      shiftwidth = 2;
+    };
+
     colorschemes.gruvbox = {
       enable = true;
     };
+
+    clipboard.providers.wl-copy.enable = true;
+
     plugins = {
+      treesitter.enable = true;
       comment-nvim.enable = true;
-      emmet.enable = true;
+      # emmet.enable = true;
       nvim-autopairs.enable = true;
       bufferline.enable = true;
-      fugitive.enable = true;
-      gitgutter.enable = true;
-      gitsigns.enable = true;
+      which-key.enable = true;
+      nix.enable = true;
+
+      # fugitive.enable = true;
+      # gitgutter.enable = true;
+      # gitsigns.enable = true;
 
       airline = {
         enable = true;
@@ -25,10 +42,19 @@
         recommendedKeymaps = true;
       };
 
+      telescope = {
+        enable = true;
+        highlightTheme = "gruvbox";
+        extensions = {
+          fzf-native.enable = true;
+        };
+      };
+
       lsp = {
         enable = true;
         servers = {
           gopls.enable = true;
+          nil_ls.enable = true;
         };
       };
     };
