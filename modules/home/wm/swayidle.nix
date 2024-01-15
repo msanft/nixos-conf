863814c4147c7d4fbc4c31c49fd24db16b8aef83
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2024-01-15 16:44:11 +0100

    swayidle: enable

--- modules/home/wm/swayidle.nix
+++ modules/home/wm/swayidle.nix
@@ -1,5 +1,22 @@
-{ ... }: {
+{ pkgs, ... }: {
   services.swayidle = {
     enable = true;
+    events = [
+      {
+        event = "before-sleep";
+        command = "swaylock -f";
+      }
+    ];
+    timeouts = [
+      {
+        timeout = 240;
+        command = "swaylock -f";
+      }
+      {
+        timeout = 600;
+        command = "swaymsg 'output * power off'";
+        resumeCommand = "swaymsg 'output * power on'";
+      }
+    ];
   };
 }
