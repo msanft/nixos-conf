a2343fbc23757d1d3e341f9896c52db32e563bbe
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2024-01-15 16:42:09 +0100

    flake: update

--- flake.nix
+++ flake.nix
@@ -3,8 +3,9 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-unstable";
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
     };
+    fix-fork.url = "github:msanft/nixpkgs/msanft/ms-vsliveshare";
     berkeley-mono = {
       url = "git+ssh://git@github.com/msanft/berkeley-mono";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -27,33 +28,43 @@
     };
   };
 
-  outputs = inputs: with inputs; {
+  outputs = inputs:
+    let
+      overlay-fix-fork = final: prev: {
+        fix-fork = import inputs.fix-fork {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+        };
+      };
+    in
+    with inputs; {
 
-    nixosConfigurations.tp = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./hosts/tp/configuration.nix
-        ./hosts/tp/hardware-configuration.nix
-        home-manager.nixosModules.home-manager
-        disko.nixosModules.disko
-        ./hosts/tp/disko.nix
-        {
-          home-manager.users.moritzs.imports = [
-            nixvim.homeManagerModules.nixvim
-          ];
-        }
-      ];
-      specialArgs = { inherit inputs; };
+      nixosConfigurations.tp = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-fix-fork ]; })
+          ./hosts/tp/configuration.nix
+          ./hosts/tp/hardware-configuration.nix
+          home-manager.nixosModules.home-manager
+          disko.nixosModules.disko
+          ./hosts/tp/disko.nix
+          {
+            home-manager.users.moritzs.imports = [
+              nixvim.homeManagerModules.nixvim
+            ];
+          }
+        ];
+        specialArgs = { inherit inputs; };
+      };
+
+      darwinConfigurations.mb = darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        modules = [
+          ./hosts/mb/configuration.nix
+          home-manager.darwinModules.home-manager
+
+        ];
+      };
+
     };
-
-    darwinConfigurations.mb = darwin.lib.darwinSystem {
-      system = "aarch64-darwin";
-      modules = [
-        ./hosts/mb/configuration.nix
-        home-manager.darwinModules.home-manager
-
-      ];
-    };
-
-  };
 }
