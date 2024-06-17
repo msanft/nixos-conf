6f83c03d2b60864d5535f27fe370969f65af946a
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2024-06-17 20:46:57 +0200

    networking: use resolved

--- modules/system/networking/default.nix
+++ modules/system/networking/default.nix
@@ -1,6 +1,11 @@
 { ... }: {
+  services.resolved = {
+    enable = true;
+  };
+
   networking = {
     networkmanager.enable = true;
+
     extraHosts = ''
       127.0.0.1    license.confidential.cloud
       127.0.0.1    mastodon.local
