02da9e207ab9fd23aa029ab65e4e45aa93ec3884
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2024-07-26 20:51:17 +0200

    nixpkgs: add overlays

--- modules/nix/nixpkgs.nix
+++ modules/nix/nixpkgs.nix
@@ -1,13 +1,13 @@
-{ pkgs, ... }: {
+{ ... }: {
   nixpkgs = {
     config = {
       allowUnfree = true;
       nvidia.acceptLicense = true;
-      permittedInsecurePackages = [ 1"nix-2.16.2" ];
     };
 
     overlays = [
       (import ../../overlays/mkDesktopApp.nix)
+      (import ../../overlays/packages.nix)
     ];
   };
 }
