71de8949ccb43b3be6a569e078292dd5e4e0cbae
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2024-07-26 20:51:29 +0200

    server: remove sound option

--- modules/nixos/server.nix
+++ modules/nixos/server.nix
@@ -90,7 +90,6 @@
   time.timeZone = lib.mkDefault "UTC";
 
   fonts.fontconfig.enable = lib.mkDefault false;
-  sound.enable = false;
 
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
