4af4fe5c0ebc1ef5f35f2205b341660303856f33
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2024-11-18 16:51:33 +0100

    modules/nvidia: drop unnecessary options

--- modules/system/nvidia/default.nix
+++ modules/system/nvidia/default.nix
@@ -7,11 +7,7 @@
   services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
     open = true;
-    nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.production;
   };
 
