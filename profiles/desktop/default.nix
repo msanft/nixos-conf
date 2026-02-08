8c1f31a2fdf077395555384b10b965cc9b01746d
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2026-02-08 22:23:21 +0100

    perfectly fine commit

--- profiles/desktop/default.nix
+++ profiles/desktop/default.nix
@@ -70,7 +70,7 @@
 
   networking.extraHosts = ''
     127.0.0.1    license.confidential.cloud
-    127.0.0.1    mastodon.local
+    127.0.0.1    ampcode.com
   '';
 
   systemd.services.autorandr.after = [ "multi-user.target" ];
@@ -88,6 +88,37 @@
       EgYDVR0TAQH/BAgwBgEB/wIBATAdBgNVHQ4EFgQUyIUXVXnou1Ag8mgxQZG457aP
       TUQwCgYIKoZIzj0EAwIDSAAwRQIgI/4E9l0uPu4w+KEVzOMBz7F1HWL2fZYHF9We
       CjhfZMYCIQCA1rWQ2ixNkCwVeF29QgfZDxmhlwBisYuDh1MhreMdBw==
+      -----END CERTIFICATE-----
+    ''
+    # mkcert
+    ''
+      -----BEGIN CERTIFICATE-----
+      MIIEvTCCAyWgAwIBAgIQYFg6ymHudCLKfn7eOHKYjzANBgkqhkiG9w0BAQsFADB3
+      MR4wHAYDVQQKExVta2NlcnQgZGV2ZWxvcG1lbnQgQ0ExJjAkBgNVBAsMHW1zYW5m
+      dEBiYXN0aW9uIChNb3JpdHogU2FuZnQpMS0wKwYDVQQDDCRta2NlcnQgbXNhbmZ0
+      QGJhc3Rpb24gKE1vcml0eiBTYW5mdCkwHhcNMjYwMTIwMDc1MzE2WhcNMzYwMTIw
+      MDc1MzE2WjB3MR4wHAYDVQQKExVta2NlcnQgZGV2ZWxvcG1lbnQgQ0ExJjAkBgNV
+      BAsMHW1zYW5mdEBiYXN0aW9uIChNb3JpdHogU2FuZnQpMS0wKwYDVQQDDCRta2Nl
+      cnQgbXNhbmZ0QGJhc3Rpb24gKE1vcml0eiBTYW5mdCkwggGiMA0GCSqGSIb3DQEB
+      AQUAA4IBjwAwggGKAoIBgQCuKRlozBwhY0zb2HWfhvGErt4BlUhMTcdcc8aQMdmv
+      HVkhLPRC+7x+Q/PHxynU5IMyvJt1QlqssBsQzUVR9+XGv9uVags1VzTyVfTkO5bM
+      /TSNbx9f+lsMZ0D5QX1aXu5oOdQyXpsncrN19Rf5al7zlhiPgEZEL58kOoKjmg4f
+      BpNcMpG4t21kNhbpHym0zo/ATtm1M6hlJ5+C031WbWf2/ODi5Yur79thdqmty9c2
+      8TCStoZx+oesmKJQolwK4BqqFzLh+4+mA7v5EptyDU6T0cng2QtMSEF3u55+/hCZ
+      wneA39+Uw1ZcskTkGD6H8rmap8UeNkMuFyejws6DJ+BsXPvV46PZzMbgi23gwLat
+      ExfJ7Bw6/449G2VB88sL/mg2yao3xUZ4Rs5kS/JVHEWJ7NvGqoQGnFGjVsXuO4yZ
+      +Yokm/foo4qrKRKCrK5+QcDUtA4hGBMtzTs2Xh/nrJRUZ4nOVgvdhyhKsz9GTS0r
+      uJdf07DtFDVmOqmftKQH7f8CAwEAAaNFMEMwDgYDVR0PAQH/BAQDAgIEMBIGA1Ud
+      EwEB/wQIMAYBAf8CAQAwHQYDVR0OBBYEFG5XJ/h1v2aGNQIKOcy1h9cGTWfWMA0G
+      CSqGSIb3DQEBCwUAA4IBgQCM2C5ESxbbeXThlIwldCymPVx642YISkeDwW5eQg4d
+      TK90PrzuUnarbUEKgY1f2VLre0ffzNSQDa2By6Z8BUh8EJQtwJI+m5fuv1XzDXOX
+      MHlmHM/b9ooobKexJq+2TnqupflOpBf8Aic+Us/e5k6VjkiGBLHxYcYL555mP0qA
+      e26q/UpnImTVU4bfwbKFpwdDc3MX3Bj/arLo5aIRGxcguqmDtdTJmqTPH80XpoUF
+      Jh4HQ3tOTEnYq/e1d+4asj5M0auk4A9CN8yxU60Ads5TxGD6cJhIp+ZMkdALRJLu
+      3x/FA4D43HsExbFx99vkNj8WQhU1zhpm3r1Hs9r0WsJTFTvdk1Z87mplmUt2Hq9c
+      77WKX945wj9rrDMguyqgw7VcSgpJ/qtFY961jun7ijhklKhijkDS29x5RavfxFI/
+      QqgmAMcRuzo/Win/10qMoGdjX0gF8Ena+BUQSQwp4VB9l0I1mAzN0TWhC7pDYDqM
+      oypgQ5HRlZ8N1E/qIkAoA6w=
       -----END CERTIFICATE-----
     ''
   ];
